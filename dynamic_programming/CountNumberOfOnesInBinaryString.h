5f633c31b7dde05ea6aefd1e13624accc7d7c6fc
zcoderz <usmanqd@gmail.com>
2022-11-10 20:09:06 -0600

    more code...

--- dynamic_programming/CountNumberOfOnesInBinaryString.h
+++ dynamic_programming/CountNumberOfOnesInBinaryString.h
@@ -15,11 +15,11 @@ public:
     static int numberOfDigitsWithoutConsecutiveOnes(int theNo) {
         vector<int> ones (theNo+1);
         vector<int> zeroes (theNo+1);
-        ones[0]=1;
+        ones[0]=0;
         zeroes[0]=1;
         for (int i =1; i <= theNo; i++) {
-            zeroes[i] += (zeroes[i-1] + ones[i-1]);
-            ones[i] += (zeroes[i-1] + (i-2 >=0 ? ones[i-2] : 0));
+            zeroes[i] = (zeroes[i-1] +  ones[i-1]);
+            ones[i] = (zeroes[i-1]) ;
         }
         return ones[theNo] + zeroes[theNo];
     }
@@ -29,20 +29,19 @@ public:
             return 1;
         }
         int cnt = 0;
-        if (priorOne == -1) {
+        if (priorOne == -1 || priorOne==0) {
             cnt = processRecur(no-1, 1);
             cnt += processRecur(no-1, 0);
         } else if (priorOne == 1) {
             cnt = processRecur(no-1, 0);
-        } else {
-            cnt = processRecur(no-1, 1);
         }
+        cout << "count at " << no << " with prior " << priorOne << " is " << cnt << endl;
         return cnt;
     }
 
     static void testMe() {
-        int res = processRecur(3, -1);
-        //int res = numberOfDigitsWithoutConsecutiveOnes(3);
+        //int res = processRecur(4, -1);
+        int res = numberOfDigitsWithoutConsecutiveOnes(1);
         cout << res << endl;
     }
 };
