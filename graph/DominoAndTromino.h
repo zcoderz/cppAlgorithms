507eb349f20c7d9a5874646bc52711e4240d975e
zoderz <usmanqd@gmail.com>
2023-01-14 16:51:20 -0800

    continuing practice ...

--- graph/DominoAndTromino.h
+++ graph/DominoAndTromino.h
@@ -9,7 +9,7 @@
 using namespace std;
 class DominoTromino {
     /**
-     * Excellent explanation on this at leetcode. They also explain solving this via matrix multiplication.
+     * Visualizing this one is very tricky!!!
      * https://leetcode.com/problems/domino-and-tromino-tiling/solutions/1570448/domino-and-tromino-tiling/
      * f(k): The number of ways to fully cover a board of width kkk.
      * p(k): The number of ways to partially cover a board of width kkk.
@@ -21,24 +21,27 @@ class DominoTromino {
         if (n==2) return 2;
 
         int MOD = 1000000007;
-        int fPrior=1; //only 1 way to cover the first index with a vertical bar
-        int fCurrent=2; //only 2 ways to completely cover first two indices (two vertical or two horizontal)
-        int pCurrent=1; //only 1 way to add an L in first index
-
+        int fPriorPrior=1; //only 1 way to cover the first index with a vertical bar
+        int fPrior=2; //only 2 ways to completely cover first two indices (two vertical or two horizontal)
+        int pPrior=1; //only 1 way to add an L in first index
 
         for (int i =3; i <=n; i++) {
-            int tmp = fCurrent;
-            //can get to current F by adding 1 to priorF, adding two horizontal or two vertical to the prior prior F.
-            //however, adding two vertical will double count the scenario already covered by adding one vertical to
-            //the priorF
-            //pCurrent is treated singularly for both partial up and down. hence can get to current fully covered by either of
-            //adding an upper l or lower l to priorF
-            fCurrent = (fPrior + fCurrent + 2 * pCurrent) % MOD;
-            pCurrent = (fPrior + pCurrent) % MOD; // can add L to prior prior F , or add horizontal to prior p
-            fPrior = tmp;
+            int tmp = fPrior;
+
+            //to fPriorPrior you can add two horizontal dominos
+            //to fPrior you can add one vertical domino
+            //to L you can add an inverted L
+            //to a horizontal upper domino creating a partial in prior you can add an inverted L
+            //hence 2*prior
+            fPrior = (fPriorPrior + fPrior + 2 * pPrior) % MOD;
+
+            //to a full at 2 prior you can add L
+            //to a prior L , you can add an upper domino to create L- shape
+            pPrior = (fPriorPrior + pPrior) % MOD;
+            fPriorPrior = tmp;
         }
 
-        return fCurrent;
+        return fPrior;
     }
 
 };
